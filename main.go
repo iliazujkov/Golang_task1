e41c011c3624456cc1e4ad9ffc08aa02ceacf883
iliazujkov <90790199+iliazujkov@users.noreply.github.com>
2023-10-04 23:31:37 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,27 +1,29 @@
 package main
 
 import (
-	"storage/list"
-	"storage/mp"
+	"MachineLearning_1/storage/list"
+	"MachineLearning_1/storage/mp"
 	"fmt"
 )
 
-type Storage interface {
+type storage interface {
+	NewStorage() interface{}
+	Add(value interface{}) int64
 	Len() int64
-	Add(value int64) int64
 	RemoveByIndex(id int64)
-	RemoveByValue(value int64)
-	RemoveAllByValue(value int64)
-	GetByIndex(id int64) (int64, bool)
-	GetByValue(value int64) (int64, bool)
-	GetAllByValue(value int64) (ids []int64, ok bool)
-	GetAll() (values []int64, ok bool)
+	RemoveByValue(value interface{})
+	RemoveAllByValue(value interface{})
+	GetByIndex(id int64) (interface{}, bool)
+	GetByValue(value interface{}) (int64, bool)
+	GetAllByValue(value interface{}) (ids []int64, ok bool)
+	GetAll() (values []interface{}, ok bool)
 	Clear()
 	Print()
 }
 
 func main() {
-	l := list.NewList()
+	var l storage = &list.List{}
+	l.NewStorage()
 	l.Add(12)
 	l.Add(54)
 	l.Add(3)
@@ -31,6 +33,10 @@ func main() {
 	l.Add(428)
 	l.Add(2123)
 	fmt.Println(l.GetAllByValue(14))
+	fmt.Println(l.GetAllByValue(13))
+	fmt.Println(l.GetAllByValue(12))
+	fmt.Println(l.GetAllByValue(11))
+
 	fmt.Println(l.GetAll())
 	fmt.Println(l.GetByValue(12))
 	fmt.Println(l.GetByIndex(-4))
@@ -38,16 +44,22 @@ func main() {
 	l.RemoveByIndex(4)
 	l.RemoveByValue(4)
 	l.Print()
-	m := mp.NewMap()
+	var m storage = &mp.Map{}
+	m.NewStorage()
 	m.Add(5)
 	m.Add(2)
 	m.Add(1)
 	m.Add(5)
 	m.Add(1)
 	m.Add(4)
-	//m.RemoveByIndex(3)
-	//fmt.Println(m.GetAllByValue(5))
-	//fmt.Println(m.GetByValue(5))
+	m.RemoveByIndex(3)
+	m.RemoveByIndex(3)
+	m.RemoveByIndex(0)
+	fmt.Println(m.GetAllByValue(5))
+	fmt.Println(m.GetByValue(5))
+	go fmt.Println(m.GetByIndex(1))
+	go fmt.Println(m.GetByIndex(1))
+	go fmt.Println(m.GetByIndex(1))
 	fmt.Println(m.GetAll())
 	m.Print()
 	m.Clear()
